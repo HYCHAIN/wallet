dbce62f56ad8a9444b8ecefdf650885d945caa88
ArkDev <arkdev@ArkDevs-Laptop.local>
2023-04-06 21:48:10 -0700

    Removed unused implementation slot constant

--- contracts/modules/Upgrades/Upgrades.sol
+++ contracts/modules/Upgrades/Upgrades.sol
@@ -13,8 +13,6 @@ import "./IUpgrades.sol";
 import "../Controllers/Controllers.sol";
 
 contract Upgrades is IUpgrades, Controllers {
-  bytes32 private constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-
   function upgrade(
     address _implementation,
     uint256 _nonce,
