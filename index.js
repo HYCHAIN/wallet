662fa656febafc8b10714ea5c82b3237a77940c0
ArkDev <me@braydonb.com>
2023-09-01 11:52:07 -0700

    Added postinstall script to compile contracts with hardhat, added convenience index.js function for package to allow importation and usage of compiled contract bytecode

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ * Allows importation of the package & direct usage of compiled bytecode
+ * from generated artifacts of postinstall `npx hardhat compile`.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const contracts = {};
+
+const recurseContracts = rootDirectory => {
+  const directoryItems = fs.readdirSync(rootDirectory);
+
+  for (let i = 0; i < directoryItems.length; i++) {
+    const directoryItem = directoryItems[i];
+    const itemPath = `${rootDirectory}/${directoryItem}`;
+
+    if (fs.lstatSync(itemPath).isDirectory()) {
+      recurseContracts(itemPath);
+    }
+
+    if (directoryItem.includes('.json') && !directoryItem.includes('.dbg.json')) {
+      const contractName = directoryItem.replace('.json', '');
+
+      contracts[contractName] = require(itemPath);
+    }
+  }
+}
+
+recurseContracts(path.join(__dirname, './artifacts/contracts'));
+recurseContracts(path.join(__dirname, './artifacts/@openzeppelin'));
+
+module.exports = contracts;
